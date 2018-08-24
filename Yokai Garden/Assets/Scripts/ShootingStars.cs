0077159e35ae3597e24f247325a4001e814a6465
DESKTOP-FID27GQ\hiyada <dyson.allie@gmail.com>
2018-08-23 22:26:39 -0500

    Stars shoot but beginning of shooting stars looks bad, need to fix

--- Yokai Garden/Assets/Scripts/ShootingStars.cs
+++ Yokai Garden/Assets/Scripts/ShootingStars.cs
@@ -1,4 +1,4 @@
-﻿/*using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -16,7 +16,7 @@ public class ShootingStars : MonoBehaviour
     private int currentStar = 0;
     private bool gameOver = false;
 
-    private Vector2 starPoolPosition = new Vector2(-15, -25);
+    private Vector2 starPoolPosition = new Vector2(-15f, -25f);
 
 	// Use this for initialization
 	void Start ()
@@ -50,54 +50,3 @@ public class ShootingStars : MonoBehaviour
         }
 	}
 }
-    
-using UnityEngine;
-using System.Collections;
-
-public class Bird : MonoBehaviour 
-{
-    public float upForce;                   //Upward force of the "flap".
-    private bool isDead = false;            //Has the player collided with a wall?
-
-    private Animator anim;                  //Reference to the Animator component.
-    private Rigidbody2D rb2d;               //Holds a reference to the Rigidbody2D component of the bird.
-
-    void Start()
-    {
-        //Get reference to the Animator component attached to this GameObject.
-        anim = GetComponent<Animator> ();
-        //Get and store a reference to the Rigidbody2D attached to this GameObject.
-        rb2d = GetComponent<Rigidbody2D>();
-    }
-
-    void Update()
-    {
-        //Don't allow control if the bird has died.
-        if (isDead == false) 
-        {
-            //Look for input to trigger a "flap".
-            if (Input.GetMouseButtonDown(0)) 
-            {
-                //...tell the animator about it and then...
-                anim.SetTrigger("Flap");
-                //...zero out the birds current y velocity before...
-                rb2d.velocity = Vector2.zero;
-                //  new Vector2(rb2d.velocity.x, 0);
-                //..giving the bird some upward force.
-                rb2d.AddForce(new Vector2(0, upForce));
-            }
-        }
-    }
-
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        // Zero out the bird's velocity
-        rb2d.velocity = Vector2.zero;
-        // If the bird collides with something set it to dead...
-        isDead = true;
-        //...tell the Animator about it...
-        anim.SetTrigger ("Die");
-        //...and tell the game control about it.
-        GameControl.instance.BirdDied ();
-    }
-}*/
