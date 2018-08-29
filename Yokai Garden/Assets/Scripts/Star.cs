eb8914cf11203de7bf161f767fafbdefa23a58dd
DESKTOP-FID27GQ\hiyada <dyson.allie@gmail.com>
2018-08-28 21:34:29 -0500

    Can toggle all yokai booth pieces. Star count not input.

--- Yokai Garden/Assets/Scripts/Star.cs
+++ Yokai Garden/Assets/Scripts/Star.cs
@@ -6,11 +6,24 @@ public class Star : MonoBehaviour
     public float xForce;                    //this is the sideways force the star will travel
     public float yForce;                    //this is the downward force the star will travel
 
+    public AudioSource starSFX;
+
     private Rigidbody2D starObject;         //holds a reference to the RigidBody2D component of the star object
+
+    static bool _hasAssignedOfficialAudioSource;
 
     private void Start()
     {
         starObject = GetComponent<Rigidbody2D>();
+        starSFX = GetComponent<AudioSource>();
+        if (_hasAssignedOfficialAudioSource == false)
+        {
+            _hasAssignedOfficialAudioSource = true;
+        }
+        else
+        {
+            this.starSFX.enabled = false;
+        }
     }
 
     private void Update()
@@ -19,6 +32,9 @@ public class Star : MonoBehaviour
         {
             starObject.AddForce(new Vector2(xForce, yForce));
             starObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            starSFX.pitch = Random.Range(.95f, 1.05f);
+            starSFX.volume = Random.Range(.9f, 1f);
+            starSFX.Play();
 
             GameController.instance.starCount();
         }
