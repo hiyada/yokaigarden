cfa6a943ce3a0b00d35eec9047274c25d1323511
DESKTOP-FID27GQ\hiyada <dyson.allie@gmail.com>
2018-08-28 19:50:47 -0500

    Cutting scope. Removed scrolling background for ease

--- Yokai Garden/Assets/Scripts/YokaiController.cs
+++ Yokai Garden/Assets/Scripts/YokaiController.cs
@@ -2,15 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class YokaiController : MonoBehaviour
+{
+    bool lampchanged = false;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+	public void LampToggle(bool lampchanged)
+    {
+        Debug.Log("yes");
+    }
 }
