69362d128f03bb2f52dc87d4454ed4318d512028
DESKTOP-FID27GQ\hiyada <dyson.allie@gmail.com>
2018-08-28 20:30:28 -0500

    Music is in, toggle info removed

--- Yokai Garden/Assets/Scripts/YokaiController.cs
+++ Yokai Garden/Assets/Scripts/YokaiController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class YokaiController : MonoBehaviour
 {
     bool lampchanged = false;
-
+    public GameObject lamp;
 
 	public void LampToggle(bool lampchanged)
     {
+        Vector2 lampPos = lamp.transform.position;
         Debug.Log("yes");
+        lamp.SetActive(lampchanged);
     }
 }
